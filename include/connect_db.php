23b496fdab360757aaff7311064a8117778c063f
Ismail <56081151+IsmailZain@users.noreply.github.com>
2020-11-08 13:13:02 +0530

    Changed connect_db and added game.php in games folder

--- include/connect_db.php
+++ include/connect_db.php
@@ -2,9 +2,9 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "gamedb";
     
-    
-    $conn = mysqli_connect($servername, $username, $password);
+    $conn = mysqli_connect($servername, $username, $password,$dbname);
     
     if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
