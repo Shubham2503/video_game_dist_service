b3c2d44def00f24ee293ecf346336a01a412976d
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-11-10 17:47:40 +0530

    added user account page

---
+++ account/index.php
@@ -0,0 +1,200 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: ../home/signin.php");
+        exit;
+    }
+
+    $userid = $_COOKIE["userid"];
+
+    //delete later////////
+    $severname = "localhost";
+    $username = "root";
+    $pwd = "";
+    $dbname = "gamedb";
+    $conn = new mysqli($severname, $username, $pwd, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    ///////////////////////
+
+
+    $fname = $lname = $username = $email = $game_name = $game_price = "";
+
+    $sql = "SELECT * FROM users WHERE userid = '$userid'";
+    $res = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($res);
+
+    $fname = $row["fname"];
+    $lname = $row["lname"];
+    $username = $row["username"];
+    $email = $row["email"];
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Checkout</title>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <link href="form-validation.css" rel="stylesheet">
+</head>
+
+
+<body class="bg-light">
+    <div class="container">
+
+        <div class="py-5 text-center">
+            <img class="d-block mx-auto mb-4" src="../image/logo.png" alt="" width="72" height="72">
+            <h2>Account</h2>
+            <p class="lead"></p>
+        </div>
+
+        <div class="col-md-12 order-md-1">
+            <form class="needs-validation" novalidate method="post" action="receipt.php">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="firstName">First name</label>
+                        <input type="text" class="form-control" id="firstName" placeholder="" value="<?php echo $fname?>" required>
+                        <div class="invalid-feedback">
+                            Valid first name is required.
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="lastName">Last name</label>
+                        <input type="text" class="form-control" id="lastName" placeholder="" value="<?php echo $lname?>" required>
+                        <div class="invalid-feedback">
+                            Valid last name is required.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="username">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">@</span>
+                            </div>
+                            <input type="text" class="form-control" id="username" placeholder="Username" value="<?php echo $username?>" required>
+                            <div class="invalid-feedback" style="width: 100%;">
+                                Your username is required.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="you@example.com" value="<?php echo $email?>" required>
+                        <div class="invalid-feedback">
+                            Please enter a valid email address for shipping updates.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-md-6 mb-3">
+                        <label for="dob">DOB</label>
+                        <input type="date" class="form-control" id="dob" value="" required>
+                        <div class="invalid-feedback">
+                            Please enter a valid DOB.
+                        </div>
+                    </div>
+                </div>
+                    <h4><label for="dob">Password</label></h4>
+
+                <div class="row">
+
+                    <div class="col-md-6 mb-3">
+                        <label for="pass">Password</label>
+                        <input type="password" class="form-control" id="pass" value="" required>
+                        <div class="invalid-feedback">
+                            Please enter a valid password.
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="c_pass">Confirm Password</label>
+                        <input type="password" class="form-control" id="c_pass" value="" required>
+                        <div class="invalid-feedback">
+                            Please enter a valid password.
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="row">
+          <div class="col-md-5 mb-3">
+            <label for="country">Country</label>
+            <select class="custom-select d-block w-100" id="country" required>
+              <option value="">Choose...</option>
+              <option>United States</option>
+            </select>
+            <div class="invalid-feedback">
+              Please select a valid country.
+            </div>
+          </div>
+          <div class="col-md-4 mb-3">
+            <label for="state">State</label>
+            <select class="custom-select d-block w-100" id="state" required>
+              <option value="">Choose...</option>
+              <option>California</option>
+            </select>
+            <div class="invalid-feedback">
+              Please provide a valid state.
+            </div>
+          </div>
+          <div class="col-md-3 mb-3">
+            <label for="zip">Zip</label>
+            <input type="text" class="form-control" id="zip" placeholder="" required>
+            <div class="invalid-feedback">
+              Zip code required.
+            </div>
+          </div>
+        </div> -->
+
+                <hr class="mb-4">
+                <button class="btn btn-primary btn-lg btn-block" name="update" type="submit">Update</button>
+            </form>
+        </div>
+    </div>
+
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2017-2020 Company Name</p>
+        <!-- <ul class="list-inline">
+      <li class="list-inline-item"><a href="#">Privacy</a></li>
+      <li class="list-inline-item"><a href="#">Terms</a></li>
+      <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul> -->
+    </footer>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script>
+        window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')
+    </script>
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="form-validation.js"></script>
+</body>
+
+</html>
