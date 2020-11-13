3feced9032d804c1d00ff0fd98c0e03412eba7fe
Ismail <56081151+IsmailZain@users.noreply.github.com>
2020-11-13 16:00:33 +0530

    Added new feature in accounts

--- account/index.php
+++ account/index.php
@@ -1,35 +1,35 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: ../home/signin.php");
-        exit;
-    }
+session_start();
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: ../home/signin.php");
+    exit;
+}
 
-    $userid = $_COOKIE["userid"];
+$userid = $_COOKIE["userid"];
 
-    //delete later////////
-    $severname = "localhost";
-    $username = "root";
-    $pwd = "";
-    $dbname = "gamedb";
-    $conn = new mysqli($severname, $username, $pwd, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    ///////////////////////
+//delete later////////
+$severname = "localhost";
+$username = "root";
+$pwd = "";
+$dbname = "gamedb";
+$conn = new mysqli($severname, $username, $pwd, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
+}
+///////////////////////
 
 
-    $fname = $lname = $username = $email = $game_name = $game_price = "";
+$fname = $lname = $username = $email = $game_name = $game_price = "";
 
-    $sql = "SELECT * FROM users WHERE userid = '$userid'";
-    $res = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($res);
+$sql = "SELECT * FROM users WHERE userid = '$userid'";
+$res = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($res);
 
-    $fname = $row["fname"];
-    $lname = $row["lname"];
-    $username = $row["username"];
-    $email = $row["email"];
+$fname = $row["fname"];
+$lname = $row["lname"];
+$username = $row["username"];
+$email = $row["email"];
 ?>
 
 <!doctype html>
@@ -61,122 +61,162 @@
 
 
 <body class="bg-light">
-    <div class="container">
-
-        <div class="py-5 text-center">
-            <img class="d-block mx-auto mb-4" src="../image/logo.png" alt="" width="72" height="72">
-            <h2>Account</h2>
-            <p class="lead"></p>
+<div class="row">
+        <div class="col-3">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
+                    aria-controls="v-pills-home" aria-selected="true">Home</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab"
+                    aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
+                    aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
+                    aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+            </div>
         </div>
+        <div class="col-9">
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
+                    aria-labelledby="v-pills-home-tab">
+                    <div class="container">
 
-        <div class="col-md-12 order-md-1">
-            <form class="needs-validation" novalidate method="post" action="receipt.php">
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="firstName">First name</label>
-                        <input type="text" class="form-control" id="firstName" placeholder="" value="<?php echo $fname?>" required>
-                        <div class="invalid-feedback">
-                            Valid first name is required.
+                        <div class="py-5 text-center">
+                            <img class="d-block mx-auto mb-4" src="../image/logo.png" alt="" width="72" height="72">
+                            <h2>Account</h2>
+                            <p class="lead"></p>
                         </div>
-                    </div>
-
-                    <div class="col-md-6 mb-3">
-                        <label for="lastName">Last name</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="" value="<?php echo $lname?>" required>
-                        <div class="invalid-feedback">
-                            Valid last name is required.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="username">Username</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">@</span>
+                
+                        <div class="col-md-12 order-md-1">
+                            <form class="needs-validation" novalidate method="post" action="receipt.php">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="firstName">First name</label>
+                                        <input type="text" class="form-control" id="firstName" placeholder="" value="<?php echo $fname ?>" required>
+                                        <div class="invalid-feedback">
+                                            Valid first name is required.
+                                        </div>
+                                    </div>
+                
+                                    <div class="col-md-6 mb-3">
+                                        <label for="lastName">Last name</label>
+                                        <input type="text" class="form-control" id="lastName" placeholder="" value="<?php echo $lname ?>" required>
+                                        <div class="invalid-feedback">
+                                            Valid last name is required.
+                                        </div>
+                                    </div>
+                                </div>
+                
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="username">Username</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">@</span>
+                                            </div>
+                                            <input type="text" class="form-control" id="username" placeholder="Username" value="<?php echo $username ?>" required>
+                                            <div class="invalid-feedback" style="width: 100%;">
+                                                Your username is required.
+                                            </div>
+                                        </div>
+                                    </div>
+                
+                                    <div class="col-md-6 mb-3">
+                                        <label for="email">Email</label>
+                                        <input type="email" class="form-control" id="email" placeholder="you@example.com" value="<?php echo $email ?>" required>
+                                        <div class="invalid-feedback">
+                                            Please enter a valid email address for shipping updates.
+                                        </div>
+                                    </div>
+                                </div>
+                
+                                <div class="row">
+                
+                                    <div class="col-md-6 mb-3">
+                                        <label for="dob">DOB</label>
+                                        <input type="date" class="form-control" id="dob" value="" required>
+                                        <div class="invalid-feedback">
+                                            Please enter a valid DOB.
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4><label for="dob">Password</label></h4>
+                
+                                <div class="row">
+                
+                                    <div class="col-md-6 mb-3">
+                                        <label for="pass">Password</label>
+                                        <input type="password" class="form-control" id="pass" value="" required>
+                                        <div class="invalid-feedback">
+                                            Please enter a valid password.
+                                        </div>
+                                    </div>
+                
+                                    <div class="col-md-6 mb-3">
+                                        <label for="c_pass">Confirm Password</label>
+                                        <input type="password" class="form-control" id="c_pass" value="" required>
+                                        <div class="invalid-feedback">
+                                            Please enter a valid password.
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- <div class="row">
+                          <div class="col-md-5 mb-3">
+                            <label for="country">Country</label>
+                            <select class="custom-select d-block w-100" id="country" required>
+                              <option value="">Choose...</option>
+                              <option>United States</option>
+                            </select>
+                            <div class="invalid-feedback">
+                              Please select a valid country.
                             </div>
-                            <input type="text" class="form-control" id="username" placeholder="Username" value="<?php echo $username?>" required>
-                            <div class="invalid-feedback" style="width: 100%;">
-                                Your username is required.
+                          </div>
+                          <div class="col-md-4 mb-3">
+                            <label for="state">State</label>
+                            <select class="custom-select d-block w-100" id="state" required>
+                              <option value="">Choose...</option>
+                              <option>California</option>
+                            </select>
+                            <div class="invalid-feedback">
+                              Please provide a valid state.
                             </div>
+                          </div>
+                          <div class="col-md-3 mb-3">
+                            <label for="zip">Zip</label>
+                            <input type="text" class="form-control" id="zip" placeholder="" required>
+                            <div class="invalid-feedback">
+                              Zip code required.
+                            </div>
+                          </div>
+                        </div> -->
+                
+                                <hr class="mb-4">
+                                <button class="btn btn-primary btn-lg btn-block" name="update" type="submit">Update</button>
+                            </form>
                         </div>
                     </div>
-
-                    <div class="col-md-6 mb-3">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="you@example.com" value="<?php echo $email?>" required>
-                        <div class="invalid-feedback">
-                            Please enter a valid email address for shipping updates.
-                        </div>
-                    </div>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 </div>
-
-                <div class="row">
-
-                    <div class="col-md-6 mb-3">
-                        <label for="dob">DOB</label>
-                        <input type="date" class="form-control" id="dob" value="" required>
-                        <div class="invalid-feedback">
-                            Please enter a valid DOB.
-                        </div>
-                    </div>
-                </div>
-                    <h4><label for="dob">Password</label></h4>
-
-                <div class="row">
-
-                    <div class="col-md-6 mb-3">
-                        <label for="pass">Password</label>
-                        <input type="password" class="form-control" id="pass" value="" required>
-                        <div class="invalid-feedback">
-                            Please enter a valid password.
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 mb-3">
-                        <label for="c_pass">Confirm Password</label>
-                        <input type="password" class="form-control" id="c_pass" value="" required>
-                        <div class="invalid-feedback">
-                            Please enter a valid password.
-                        </div>
-                    </div>
-                </div>
-                <!-- <div class="row">
-          <div class="col-md-5 mb-3">
-            <label for="country">Country</label>
-            <select class="custom-select d-block w-100" id="country" required>
-              <option value="">Choose...</option>
-              <option>United States</option>
-            </select>
-            <div class="invalid-feedback">
-              Please select a valid country.
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    ... Proffile</div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                    ...Mesasage</div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                    ...</div>
             </div>
-          </div>
-          <div class="col-md-4 mb-3">
-            <label for="state">State</label>
-            <select class="custom-select d-block w-100" id="state" required>
-              <option value="">Choose...</option>
-              <option>California</option>
-            </select>
-            <div class="invalid-feedback">
-              Please provide a valid state.
-            </div>
-          </div>
-          <div class="col-md-3 mb-3">
-            <label for="zip">Zip</label>
-            <input type="text" class="form-control" id="zip" placeholder="" required>
-            <div class="invalid-feedback">
-              Zip code required.
-            </div>
-          </div>
-        </div> -->
-
-                <hr class="mb-4">
-                <button class="btn btn-primary btn-lg btn-block" name="update" type="submit">Update</button>
-            </form>
         </div>
     </div>
+
 
     <footer class="my-5 pt-5 text-muted text-center text-small">
         <p class="mb-1">&copy; 2017-2020 Company Name</p>
