589f37aaa3e5c751738886101e106d5059881a86
Sahil-Shende <47074843+Sahil-Shende@users.noreply.github.com>
2020-11-22 13:45:00 +0530

    updated navbar settings

--- store/index.php
+++ store/index.php
@@ -73,10 +73,10 @@ foreach ($game_cat as $category => $game_list) {
 
 
 <body>
-    <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-
-            <a class="navbar-brand" href="#">Home</a>
+            <a class="navbar-brand" href="../store/index.php">
+                <img src="../image/logo.png" width="40" height="40" alt="home" loading="lazy">
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -84,22 +84,15 @@ foreach ($game_cat as $category => $game_list) {
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="../account/index.php">Account <span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
-                <form class="form-inline mt-2 mt-md-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <form class="form-inline my-2 my-lg-0" action="search.php" method="get">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" name="key">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
-    </header>
 
     <main role="main">
     </main>
