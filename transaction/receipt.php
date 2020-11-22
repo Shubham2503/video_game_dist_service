587bea6898edbcddf690db75ad98ac2daab77b5a
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2020-11-22 21:56:55 +0530

    updated receipt page

--- transaction/receipt.php
+++ transaction/receipt.php
@@ -2,7 +2,7 @@
 $userid = $_POST["userid"];
 $game_id = $_POST["game_id"];
 // $email = $_POST["email"];
-$email = 'ismailparsola@gmail.com';
+$email = $_POST["email"];
 $card_no = $_POST["card_no"];
 $error = "";
 
@@ -19,17 +19,18 @@ $sql = "SELECT * FROM user_games WHERE userid = '$userid' and game_id = '$game_i
 $res = mysqli_query($conn, $sql);
 if (mysqli_num_rows($res) > 0) {
     $error = "user already owns game";
-    die ("something went wrong");
+    die("something went wrong");
     echo $error;
 }
 
 if (empty($error)) {
 
+    $result;
     $sql = "INSERT INTO user_games (userid, game_id, card_no) VALUES ('$userid', '$game_id', '$card_no')";
     if (mysqli_query($conn, $sql)) {
-        echo "transaction successfull";
+        $result =  "transaction successfull <br> Enjoy your game, have a wonderful journy.";
     } else {
-        echo "transaction err";
+        $result =  "transaction error <br> Deducted money will be refunded within 2-4 working buisness days.";
     }
     require '../phpMailer/PHPMailerAutoload.php';
     require '../phpMailer/credentials.php';
@@ -52,9 +53,41 @@ if (empty($error)) {
     $mailContent = "testmail";
     $mail->Body = $mailContent;
     if ($mail->send()) {
-        echo 'Message has been sent';
+        //echo 'Message has been sent';
     } else {
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
 }
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Receipt</title>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="receipt.css" rel="stylesheet">
+    <style>
+        
+        body {
+            background: radial-gradient(#000, #000) rgba(34, 34, 40, 0.94);
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="result-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+            <h1 class="display-4">Receipt</h1>
+            <p class="lead"><?php echo $result; ?></p>
+            <form action="../store/index.php"><button class="btn btn-outline-warning btn-lg btn-block" type="submit">Return To Store</button></form>
+        </div>
+    </div>
+</body>
+
+</html>
